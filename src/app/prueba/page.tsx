7dd0a7727d67dfd024d952e1bfed7c64f7df8f91
Daniel Perez <dperezg23@outlook.com>
2025-10-22 23:10:13 -0500

    cars style

--- src/app/prueba/page.tsx
+++ src/app/prueba/page.tsx
@@ -4,9 +4,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h1>Crea tu web</h1>
-            <p>Aquí puedes empezar a crear tu propia página web.</p>
-            <CardStyleServices/>
+            <CardStyleServices />
         </div>
     );
 };
